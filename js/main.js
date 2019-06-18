854c09c42fd9d534c6c224903adca46ca5220070
TIMUR <1tkurbanov@gmail.com>
2019-06-18 09:28:15 -0400

    d

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-
+document.addEventListener('DOMContentLoaded', function(){
+    const statusHeading = document.querySelector('.status');
+    statusHeading.innerText = "SWITCH OFF THE LIGHT, IT'S BRIGHT!";
+    const lightSwitch = document.querySelector('button');
+    const body = document.querySelector('body');
+  
+    lightSwitch.addEventListener('click', flipSwitch);
+  
+    function flipSwitch(event) {
+      if (event.target.classList.contains('on')) {
+        event.target.classList.remove('on');
+        event.target.classList.add('off');
+        body.className = 'dark';
+        statusHeading.innerText = "who turned off the light?";
+      } else {
+        event.target.classList.remove('off');
+        event.target.classList.add('on');
+        body.className = 'light';
+        statusHeading.innerText = "It's too bright in here!";
+      }
+    }
+  
+  });
+  
 
